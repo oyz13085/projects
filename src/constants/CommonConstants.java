ab4d4a38e5006205e396924646b358dad85e3a62
oyz13085 <oyz13085@gmail.com>
2025-11-01 12:58:54 +0800

    Remove credentials :P

--- src/constants/CommonConstants.java
+++ src/constants/CommonConstants.java
@@ -3,6 +3,6 @@ package constants;
 public class CommonConstants {
     public static final String DB_URL = "jdbc:mysql://localhost:3306/bankingdb";
     public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "OoiZhe9@";
+    public static final String DB_PASSWORD = "password";
     public static final String DB_USERS_TABLE_NAME = "users";
 }
